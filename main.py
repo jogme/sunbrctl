6c30f9870a4347a280c0b5068badf34585e14ca7
Norbert Pocs <norbertpocs0@gmail.com>
2024-02-20 09:13:45 +0100

    Add new option to set value
    Signed-off-by: Norbert Pocs <norbertpocs0@gmail.com>

--- main.py
+++ main.py
@@ -15,10 +15,19 @@ if __name__ == "__main__":
                         description='automatic monitor brightness control')
     parser.add_argument('-v', action='store_true', help='Enable debug logs')
     parser.add_argument('-e', '--external', action='store_true', help='Enable external monitor')
+    parser.add_argument('-s', '--set', type=int, help='Set brightness. If used with external option, \
+                        sets the external monitors brightness. The value should be given as a percentage \
+                        (0-100).')
     args = parser.parse_args()
 
-    mediator = Mediator(args.v, args.external)
+    if args.set:
+        if args.set < 0 or args.set > 100:
+            print('The set value is out of bound.', file=stderr)
+            exit(-1)
+        # FIXME send the set signal for the main prog if running
+    else:
+        mediator = Mediator(args.v, args.external)
 
-    while True:
-        mediator.updater.update()
-        sleep(config.sleep_time_s)
+        while True:
+            mediator.updater.update()
+            sleep(config.sleep_time_s)
